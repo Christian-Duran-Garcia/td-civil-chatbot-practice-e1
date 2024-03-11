62e936103f870fc06e57661243dc5746876e1d06
Christian-Duran-Garcia <106398151+Christian-Duran-Garcia@users.noreply.github.com>
2024-03-11 14:37:41 -0600

    ngrok chatbot

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ install(library_to_install)
 
 
 def on_input_change():
-    url = "https://63ad-131-178-102-200.ngrok-free.app/question"
+    url = "https://c58b-131-178-102-200.ngrok-free.app/question"
     user_input = st.session_state.user_input
     question = {"question":user_input}
     headers = {'ngrok-skip-browser-warning':'true', 'Content-Type':'application/json'}
